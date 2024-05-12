8ad0824bfdfee51f5ca027aa617020974730b539
KovaDav <kovacs.david990420@gmail.com>
2024-05-12 10:44:04 +0200

    Refactor: rebuilding frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./Pages/Layout/Layout.js";
-import LeaderboardPage from './Pages/LeaderboardPage.js';
-
-
-const router = createBrowserRouter([
-  {
-  path: "/",
-  element: <Layout />,
-  children:[
-    {
-      path: "/",
-      element: <LeaderboardPage />
-    }
-  ]
-}
-  ]
-      
-    
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
