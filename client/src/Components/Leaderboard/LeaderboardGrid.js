388927a1ff8a971f5b6926e1f6a71b9c1233be9d
KovaDav <kovacs.david990420@gmail.com>
2024-06-02 10:00:49 +0200

    Bugfix: leaderboard page can be viewed even if there is not enough data for 3 placements

--- client/src/Components/Leaderboard/LeaderboardGrid.js
+++ client/src/Components/Leaderboard/LeaderboardGrid.js
@@ -9,15 +9,15 @@ const LeaderboardGrid = ({ title, data }) => {
       <p id="HeaderName" className="Grid">name</p>
       <p id="HeaderDPS" className="Grid">DPS</p>
       <p id="HeaderSupport" className="Grid">support</p>
-      <p className="Name1 Grid">{data == null ? "N/A" : data[0].charactername}</p>
-      <p className="Name2 Grid">{data == null ? "N/A" : data[1].charactername}</p>
-      <p className="Name3 Grid">{data == null ? "N/A" : data[2].charactername}</p>
-      <p className="DPS1 Grid">{data == null ? "N/A" : data[0].dps}</p>
-      <p className="DPS2 Grid">{data == null ? "N/A" : data[1].dps}</p>
-      <p className="DPS3 Grid">{data == null ? "N/A" : data[2].dps}</p>
-      <p className="Support1 Grid">{data === null ? 'N/A' : data[0].support}</p>
-      <p className="Support2 Grid">{data === null ? 'N/A' : data[1].support}</p>
-      <p className="Support3 Grid">{data === null ? 'N/A' : data[2].support}</p>
+      <p className="Name1 Grid">{data == null ? "N/A" : data[0] === undefined ? "N/A" : data[0].charactername}</p>
+      <p className="Name2 Grid">{data == null ? "N/A" : data[1] === undefined ? "N/A" : data[1].charactername}</p>
+      <p className="Name3 Grid">{data == null ? "N/A" : data[2] === undefined ? "N/A" : data[2].charactername}</p>
+      <p className="DPS1 Grid">{data == null ? "N/A" : data[0] === undefined ? "N/A" : data[0].dps}</p>
+      <p className="DPS2 Grid">{data == null ? "N/A" : data[1] === undefined ? "N/A" : data[1].dps}</p>
+      <p className="DPS3 Grid">{data == null ? "N/A" : data[2] === undefined ? "N/A" : data[2].dps}</p>
+      <p className="Support1 Grid">{data === null ? 'N/A' : data[0] === undefined ? "N/A" : data[0].support}</p>
+      <p className="Support2 Grid">{data === null ? 'N/A' : data[1] === undefined ? "N/A" : data[1].support}</p>
+      <p className="Support3 Grid">{data === null ? 'N/A' : data[2] === undefined ? "N/A" : data[2].support}</p>
     </div>}
         </>
     );
